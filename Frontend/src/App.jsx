3ecd610a8bc7939d7284375218367d6191cf35e2
Kalpa chamathkara Punchihewa <122891454+KalpaChamathkara@users.noreply.github.com>
2025-02-19 00:40:19 +0530

    product page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import NewsLine from "./components/newsline.jsx";
 import Home from "./pages/Home.jsx";
-import ProductsPage from "./pages/productspage.jsx";
+import ProductsPage from "./pages/Productspage.jsx";
 import SignupPage from "./pages/Signup.jsx";
 import SignInPage from "./pages/Signin.jsx";
 
